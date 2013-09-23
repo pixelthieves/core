f9cfd36f11e49997b43e6f7ac1169f2550b72a9c
xkings <xkingsgames@gmail.com>
2013-09-24 01:09:14 +0200

    Refactoring.

--- src/com/xkings/core/component/PositionOffsetComponent.java
+++ src/com/xkings/core/component/PositionOffsetComponent.java
@@ -8,18 +8,16 @@ public class PositionOffsetComponent extends Component {
     private final Vector3 point;
 
     public PositionOffsetComponent(float x, float y, float z) {
-        this.point = new Vector3(x, y, z);
+        this(new Vector3(x, y, z));
+    }
+
+    public PositionOffsetComponent() {
+        this(new Vector3());
     }
 
     public PositionOffsetComponent(Vector3 point) {
         this.point = new Vector3(point);
     }
-
-
-    public PositionOffsetComponent() {
-        this.point = new Vector3();
-    }
-
 
     public Vector3 getPoint() {
         return point;

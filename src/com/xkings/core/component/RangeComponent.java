29eb878ee65820f863b6d70a0b7ff70ee47e7dcd
xkings <xkingsgames@gmail.com>
2013-10-11 15:53:13 +0200

    Tower range is only visible while selecting or placing tower.

--- src/com/xkings/core/component/RangeComponent.java
+++ src/com/xkings/core/component/RangeComponent.java
@@ -5,12 +5,27 @@ import com.artemis.Component;
 public class RangeComponent extends Component {
 
     private final float range;
+    private boolean visible;
 
     public RangeComponent(float range) {
+        this(range, false);
+
+    }
+
+    public RangeComponent(float range, boolean visible) {
         this.range = range;
+        this.visible = visible;
     }
 
     public float getRange() {
         return range;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 }

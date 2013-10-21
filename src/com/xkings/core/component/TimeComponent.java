63a323375d57847c222008cd151947a37a1e9c3f
xkings <xkingsgames@gmail.com>
2013-10-22 01:48:17 +0200

    Bubble attack added.

--- src/com/xkings/core/component/TimeComponent.java
+++ src/com/xkings/core/component/TimeComponent.java
@@ -1,15 +1,24 @@
 package com.xkings.core.component;
 
 import com.artemis.Component;
+import com.artemis.EntitySystem;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Tomas on 9/8/13.
  */
 public class TimeComponent extends Component {
 
-    private Time time = new Time();
+    private final Map<Class<? extends EntitySystem>, Time> map = new HashMap<Class<? extends EntitySystem>, Time>();
 
-    public Time getTime() {
-        return time;
+    public Time getTime(Class<? extends EntitySystem> clazz) {
+        Time result = map.get(clazz);
+        if (result == null) {
+            result = new Time();
+            map.put(clazz, result);
+        }
+        return result;
     }
 }

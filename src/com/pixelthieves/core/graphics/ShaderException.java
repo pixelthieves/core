bfb1b811b177cb1fa7deaf7100562dbd3bd41183
xkings <xkingsgames@gmail.com>
2013-11-18 23:46:05 +0100

    On some hardware configuration, log info is empty.

--- src/com/pixelthieves/core/graphics/ShaderException.java
+++ src/com/pixelthieves/core/graphics/ShaderException.java
@@ -11,9 +11,21 @@ public class ShaderException extends RuntimeException {
      *
      */
     private static final long serialVersionUID = -4694794243512334997L;
+    private static final String emptyLog = "--From Fragment Shader:\n--From Vertex Shader:\n";
 
-    public ShaderException(String name, ShaderProgram shader) {
-        super("Compilation of shader [" + name + "] was unsuccessful.\n" +
-                shader.getLog());
+    public ShaderException(String name, String log) {
+        super("Compilation of shader [" + name + "] was unsuccessful.\n"+log);
     }
+
+    public static ShaderException getInstance(String name, ShaderProgram shader){
+        String log = shader.getLog();
+        if(log.equals(emptyLog)){
+           /* AndroidGL20 gl2 = new AndroidGL20();
+            Log.e(TAG, gl2.glGetShaderInfoLog(shader));  */
+
+            log = "No log information found, this is a bug on android.";
+        }
+        return new ShaderException(name, log);
+    }
+
 }

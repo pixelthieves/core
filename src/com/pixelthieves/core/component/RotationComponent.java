c8a8dfbb2b6b9a282634318e1ee06081b6df2bc5
xkings <xkingsgames@gmail.com>
2013-12-04 15:49:47 +0100

    Bug fixes.

--- src/com/pixelthieves/core/component/RotationComponent.java
+++ src/com/pixelthieves/core/component/RotationComponent.java
@@ -8,6 +8,7 @@ public class RotationComponent extends Component {
 
     private final Vector3 rotation;
     private Vector3 origin = new Vector3(0, 0, 0);
+    private Vector3 offset = new Vector3(0, 0, 0);
 
     public RotationComponent() {
         this(0, 0, 0);
@@ -23,5 +24,15 @@ public class RotationComponent extends Component {
 
     public Vector3 getOrigin() {
         return origin;
+    }
+
+    public static RotationComponent getInstanceFromOffset(float x, float y, float z) {
+        RotationComponent rotation = new RotationComponent();
+        rotation.offset.set(x, y, z);
+        return rotation;
+    }
+
+    public void addOffset() {
+        this.rotation.add(offset);
     }
 }

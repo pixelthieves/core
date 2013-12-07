a3989147467f1fdf494d7e274a2a400169ba29aa
xkings <xkingsgames@gmail.com>
2013-12-07 15:46:07 +0100

    Application is closed properly after add is shown.

--- src/com/pixelthieves/core/services/AdService.java
+++ src/com/pixelthieves/core/services/AdService.java
@@ -1,10 +1,12 @@
 package com.pixelthieves.core.services;
 
+import java.util.concurrent.Callable;
+
 /**
  * Created by Tomas on 12/7/13.
  */
 public interface AdService {
 
-    public void cacheAds();
-    public void showAd();
+    public void initAds();
+    public void showAd(Callable handler);
 }
